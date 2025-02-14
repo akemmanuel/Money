61ad0e9facd2ccc6425fc657b9c469578777ccec
Akemmanuel <akemsoft@gmail.com>
2025-02-14 12:41:30 +0100

    Refactor login form and guest layout for improved styling and accessibility

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,34 +14,30 @@
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
-
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+  <label class="label label-text" for="email">Email</label>
+  <input type="text" id="email" class="input" type="email" name="email" :value="old('email')" required autofocus  />
+</div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <label class="label label-text" for="password">Passwort</label>
+            <input id="password" class="input" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+            <div class="flex items-center gap-1">
+  <input type="checkbox" class="checkbox" id="defaultCheckbox1" name="remember"/>
+  <label class="label label-text text-base" for="defaultCheckbox1">Remember me</label>
+</div>
 
-            <div class="flex items-center justify-end mt-4">
+
+            <div class="flex items-center justify-end mt-4 gap-1">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="link" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+                <button class="btn btn-primary">Login</button>
 
-                <x-button class="ml-4">
-                    {{ __('Login') }}
-                </x-button>
             </div>
         </form>
 
