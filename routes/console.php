d4556f6339a1b028290abe68adf85eea47ea2b8a
Akemmanuel <akemsoft@gmail.com>
2025-02-28 20:32:11 +0100

    Add notification model and migration; schedule Bitcoin price updates

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
+use App\Models\Notification;
+
+Schedule::call(function () {
+    $client = new \GuzzleHttp\Client();
+    $response = $client->get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true');
+    $data = json_decode($response->getBody(), true);
+    $change = $data['bitcoin']['usd_24h_change'];
+
+    Notification::create([
+        'title' => "Bitcoin ist um " . $change . " " , ]);
+. ($change > 0 ? 'gestiegen' : 'gefallen')
+})->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
