545c074525dba4bd790e3a072db4734423758524
Akemmanuel <akemsoft@gmail.com>
2025-04-06 16:19:41 -0300

    chore: update dependencies and remove unused packages
    - Removed multiple esbuild platform-specific packages (e.g., aix-ppc64, android-arm64, darwin-arm64, etc.) to streamline the dependency tree.
    - Updated @floating-ui/core to version 1.6.9 and added @floating-ui/dom and @floating-ui/utils as dependencies.
    - Removed outdated packages such as @popperjs/core and rollup platform-specific packages.
    - Updated postcss-selector-parser to version 6.1.2 and cleaned up related dependencies.
    - Updated flyonui to version 2.0.1 and adjusted its dependencies accordingly.
    - Cleaned up various other dependencies and their versions for better compatibility and performance.

--- app/Livewire/EditAccount.php
+++ app/Livewire/EditAccount.php
@@ -2,19 +2,79 @@
 
 namespace App\Livewire;
 
+use App\Models\Fiat;
+use App\Models\Price;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class EditAccount extends Component
 {
     public $account;
+    public $transactions;
+
+    public $transactionName;
+    public $transactionAmount;
+    public $transactionDescription;
 
     public function mount($id)
     {
         $this->account = Auth::user()->accounts()->find($id);
+        $this->transactions = $this->account->transactions()->orderBy('created_at', 'asc')->get();
     }
     public function render()
     {
         return view('livewire.edit-account');
+    }
+
+    public function increase()
+    {
+        $this->account->transactions()->create([
+            'amount' => $this->transactionAmount,
+            'type' => 'increase',
+            'title' => $this->transactionName,
+            'description' => $this->transactionDescription,
+        ]);
+        $this->account->balance = $this->account->balance + $this->transactionAmount;
+        $this->account->save();
+        session()->flash('message', 'Transaction created successfully.');
+        $this->reset(['transactionName', 'transactionAmount', 'transactionDescription']);
+    }
+    public function decrease()
+    {
+        $this->account->transactions()->create([
+            'amount' => $this->transactionAmount,
+            'type' => 'decrease',
+            'title' => $this->transactionName,
+            'description' => $this->transactionDescription,
+        ]);
+        $this->account->balance = $this->account->balance - $this->transactionAmount;
+        $this->account->save();
+        session()->flash('message', 'Transaction created successfully.');
+        $this->reset(['transactionName', 'transactionAmount', 'transactionDescription']);
+    }
+    public function deleteTransaction($transactionId)
+    {
+        $transaction = $this->account->transactions()->find($transactionId);
+        if ($transaction) {
+            $this->account->balance = $this->account->balance - $transaction->amount;
+            $this->account->save();
+            $transaction->delete();
+            session()->flash('message', 'Transaction deleted successfully.');
+        } else {
+            session()->flash('error', 'Transaction not found.');
+        }
+    }
+    public function convert($balance, $currency, $type)
+    {
+        $currency_user =  Auth::user()->display_currency;
+        if ($currency_user == $currency) {
+            return $balance;
+        }
+        $price = new Price();
+        $priceUsd = $price->getPriceUsd($currency, $type) * $balance;
+        $fiat = new Fiat();
+        $end = $fiat->usdTo($currency_user) * $priceUsd;
+
+        return $end;
     }
 }
--- resources/views/livewire/edit-account.blade.php
+++ resources/views/livewire/edit-account.blade.php
@@ -1,18 +1,62 @@
 <div class="text-base-content">
-    <div class="card bg-base-200 shadow-md p-6">
-    
-        <h2 class="text-xl font-bold mb-4">{{$account->name}}</h2>
-        <p class="text-sm text-gray-600 mb-2">{{$account->description}}</p>
-        
-        <div class="flex space-x-4 mt-4">
-            <button class="btn btn-secondary">Movement</button>
-            <button class="btn btn-accent">Edit</button>
+    @if (session()->has('message'))
+        <div class="alert alert-success shadow-lg mb-4">
+            <div>
+                <span>{{ session('message') }}</span>
+            </div>
         </div>
-        <div class="mt-4">
-            <p><span class="font-semibold">Currency:</span> {{$account->currency}}</p>
-            <p><span class="font-semibold">Balance:</span> {{$account->balance}}</p>
-            <p><span class="font-semibold">Type of Currency:</span> {{$account->type_of_currency}}</p>
+    @endif
+    <div class="flex justify-between items-center mb-2">
+        <div>
+            <h1 class="text-2xl font-semibold text-base-content">{{$account->name}}</h1>
         </div>
     </div>
+        <p class="text-sm text-gray-600 mb-2">{{$account->description}}</p>
+
+        <div class="mt-4">
+            @if ((Auth::user()->display_currency == $account->currency && $account->type_of_currency == 'fiats') || 
+                 (Auth::user()->display_currency == 'BTC' && $account->type_of_currency == 'crypto' && $account->currency == 'BTC'))
+                <p><span class="font-semibold">Balance:</span> {{$account->balance}}{{$account->currency}}</p>
+            @else
+                <p><span class="font-semibold">Balance:</span> {{$account->balance}}{{$account->currency}}, {{ number_format($this->convert($account->balance, $account->currency, $account->type_of_currency), 2) }} {{ Auth::user()->display_currency }}</p>
+            @endif
+
+            <p><span class="font-semibold">Type of Currency:</span> {{$account->type_of_currency}}</p>
+        </div>
+<div>
+<h1 class="text-2xl font-bold mt-6 mb-4">Add Transacion</h1>
+<input class="input input-bordered w-full mb-4" type="text" placeholder="Transaction Name" wire:model="transactionName">
+<input class="input input-bordered w-full mb-4" type="text" placeholder="Transaction Description" wire:model="transactionDescription">
+<input class="input input-bordered w-full mb-4" type="number" step="0.000000000000000000001" placeholder="Transaction Amount in {{$account->currency}}" wire:model="transactionAmount">
+
+<button class="btn btn-success" wire:click="increase">Increase</button>
+
+<button class="btn btn-danger" wire:click="decrease">Decrease</button>
+</div>
+<div class="overflow-x-auto mt-6">
+    <table class="table w-full">
+        <thead>
+            <tr>
+                <th>Transaction Name</th>
+                <th>Transaction Description</th>
+                <th>Transaction Amount</th>
+                <th>Transaction Type</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($transactions as $transaction)
+                <tr>
+                    <td>{{ $transaction->title }}</td>
+                    <td>{{ $transaction->description }}</td>
+                    <td>{{ $transaction->amount }}</td>
+                    <td>{{ $transaction->type }}</td>
+                    <td>
+                        <button class="btn btn-error" wire:click="deleteTransaction({{ $transaction->id }})">Delete</button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 </div>
